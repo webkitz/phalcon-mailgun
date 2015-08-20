1ca1001129511c184483076bdc91407495c3cd18
Luke Hardiman <luke@hardiman.co.nz>
2015-08-20 20:37:16 +1200

    added memcache to api calls
    added memcache to api calls to help with our ajax frontend

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -1,27 +1,77 @@
 <?php
 use Phalcon\Mvc\Controller;
+use Mailgun\Mailgun;
+
+use Phalcon\Http\Client\Request;
+
+
 class ApiController extends Controller
 {
     //holder for our api libary
     private $_api = null;
 
+    private $cache = null;      //memcache
+
     //constructor
     public function onConstruct(){
-        //disable view
+        //disable view as were not rendering
         $this->view->disable();
+
+        //setup our memcache
+        //@todo add setting in config
+        $frontCache = new \Phalcon\Cache\Frontend\Data(array(
+            "lifetime" => 300
+        ));
+        //Create the Cache setting memcached connection options
+        $this->cache = new \Phalcon\Cache\Backend\Memcache($frontCache, array(
+            'host' => 'localhost',
+            'port' => 11211,
+            'persistent' => false
+        ));
     }
 
+    /**
+     * GET api/
+     */
     public function indexAction()
     {
-        $this->Api()->response("Incorrect API call");
+        $this->Api()->response("Invalid Call",false);
     }
 
+    /**
+     * GET api/getList
+     */
+    public function getListAction(){
+
+    }
+
+    /**
+     * @return \Api\Response\Api|Api Singleton
+     */
     protected function Api()
     {
         if($this->_api == null)
-            $this->_api = new \Api\Response\Api();
+            $this->_api = new Api\Response\Api();
 
         return $this->_api;
     }
 
+
+    /****below this are non api calls****/
+
+    /**
+     * @param $cacheKey to be referenced against
+     * @param $data to be stored
+     */
+   private function setCache($cacheKey,$data){
+       $this->cache->save($cacheKey, $data);
+   }
+
+    /**
+     * @param $cacheKey key stored agaist
+     * @return mixed | bool false if no data from key
+     */
+    private function getCache($cacheKey){
+        return $this->cache->get($cacheKey);
+    }
 }
