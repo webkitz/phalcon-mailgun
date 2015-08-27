f6d1b94f66e3aebcfe2f2c455e3d4abad6f60df4
Luke Hardiman <luke@hardiman.co.nz>
2015-08-27 23:51:02 +1200

    Retrieving mailinglists
    retrieving mail lists into databales

--- public/js/index.js
+++ public/js/index.js
@@ -1,30 +1,44 @@
 /**
  * Created by Luke Hardiman on 20/08/2015.
  */
+
+var mailingListsTable = $('#mailingLists').DataTable( {
+    "columns": [
+        { "data": "name" },
+        { "data": "description" },
+        { "data": "address" },
+        { "data": "members_count" }
+
+    ]
+} );
+
 $(document).ready(function(){
 
+
     /**
-     * ViewLists shows all the mailinglists
+     * ViewLists shows all the mailinglists on tab
      */
-    $("#viewLists").click(function(){
-        $.getJSON('api/getLists',function(response){
-                //load into grid
-                if (typeof response.data.http_response_body == "object" && response.data.http_response_body.items.length > 0){
-                    console.log("response",response.data.http_response_body.items);
-                        $('#mailingLists').DataTable( {
-                            "data": response.data.http_response_body.items,
-                            "columns": [
-                                { "data": "name" },
-                                { "data": "description" },
-                                { "data": "address" },
-                                { "data": "members_count" }
+    $("#viewLists").click(getSubscriptions);
 
-                            ]
-                        } );
+    /**
+     * Trigger mailing list render current users
+     */
+    $('#mailingLists tbody').on('click', 'tr', function () {
+        var data = mailingListsTable.row( this ).data();
+        console.log("data",data);
+    } );
 
-                }
-            });
-
-        })
-
+    //running on startup
+    getSubscriptions();
 });
+
+function getSubscriptions(){
+    $.getJSON('api/getLists',function(response){
+        //load into grid
+        if (typeof response.data.http_response_body == "object" && response.data.http_response_body.items.length > 0){
+            console.log("response",response);
+            //clear and reload
+            mailingListsTable.clear().rows.add(response.data.http_response_body.items).draw();
+        }
+    });
+}
